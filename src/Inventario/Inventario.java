eeadceb6df9b14175097b86caea463d8984aad06
Fermin <fermingarciabargas@icloud.com>
2021-05-22 16:41:09 -0300

    Metodos del inventario

--- src/Inventario/Inventario.java
+++ src/Inventario/Inventario.java
@@ -6,6 +6,22 @@ public class Inventario {
     private ArrayList<Recurso> recursos;
     private ArrayList<Herramienta> herramientas;
 
+    public void abrirInventario(){
+        for (Recurso x: this.recursos){
+            System.out.println(recursos.);
+
+        }
+
+    }
+    public void soltarItem(){
+
+    }
+    public void vaciarInventario(){
+
+    }
+    public void hayEnInventario(){
+
+    }
 
 
 }
