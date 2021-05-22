b07176a0c323be4ba630c27b198aec1086183bf9
Fermin <fermingarciabargas@icloud.com>
2021-05-22 16:33:20 -0300

    Empezando inventario y fantasma

---
+++ src/Personaje/Fantasma.java
@@ -0,0 +1,12 @@
+package Personaje;
+
+public class Enemigo extends Personaje {
+
+    public Personaje(String nombre, int vida, int damage, int resistencia, float velocidad){
+        this.nombre = Elbicho;
+        this.vida = 350;
+        this.damage = 60;
+        this.resistencia = 50;
+        this.velocidad = 10.5;
+    }
+}
