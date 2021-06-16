ee6d2a5e791b605e1a47d2ca4242f1cb35c62c0e
Fermin <fermingarciabargas@icloud.com>
2021-06-16 14:42:20 -0300

    .

--- src/RecursoNatural/Inventario.java
+++ src/RecursoNatural/Inventario.java
@@ -1,69 +1,75 @@
 package RecursoNatural;
 
 public class Inventario {
-    private Recurso recursos[];
+    private Recurso recursos[] = new Recurso[4];
     private int peso;
-    private int cantRecursosActuales=0;
 
-
-    public Inventario(){
+    public Inventario(Recurso madera, Recurso piedra, Recurso frutos, Recurso peces) {
+        this.peso = 0;
+        this.recursos[0] = madera;
+        this.recursos[1] = piedra;
+        this.recursos[2] = frutos;
+        this.recursos[3] = peces;
     }
 
-    public void agregarAlInventario(Recurso x, int cantidadAgregar){
-        int i=0;
-        if(this.cantRecursosActuales == 0) {
-            recursos[0] = x;
-            recursos[0].setCantidadRecurso(recursos[0].getCantidadRecurso() + cantidadAgregar);
-        }
-        else{
-            for (TipoRecurso aux:this.recursos) {
-                if (aux.getNombre().equals(x.getNombre())) {
-                    if (peso < 30) {
-                        recursos[i].setCantidadRecurso(recursos[i].getCantidadRecurso() + cantidadAgregar);
-                        this.peso = this.peso + cantidadAgregar;
-                    } else {
-                        System.out.println("Llevas demasiado peso!");
-                    }
-                }
-                i++;
-            }
-        }
-    }
-
-    public void verInventario(){
-        for (Recurso aux:this.recursos){
-            System.out.println("*" + aux.getNombre() + " (" + aux.getCantidadRecurso() + ")");
-        }
-    }
-
-    public void soltarRecurso(Recurso x, int cantidadTirar){
-        int i=0;
-        if(this.cantRecursosActuales == 0) {
-            System.out.println("El inventario esta vacio!");
-        }
-        else{
-            for (TipoRecurso aux:this.recursos) {
-                if (aux.getNombre().equals(x.getNombre())) {
-                    if(recursos[i].getCantidadRecurso() - cantidadTirar > 0){
-                        recursos[i].setCantidadRecurso(recursos[i].getCantidadRecurso() - cantidadTirar);
-                        this.peso = this.peso - cantidadTirar;
-                    }
-                    else{
-                        System.out.println("No puedes arrojar mas de lo que tienes!");
-                    }
-                }
-                i++;
-            }
-        }
-
-    }
-
-    public int buscarEnInventario(Recurso x){
-        for (Recurso aux:this.recursos) {
+    public void agregarAlInventario(Recurso x, int cantidadAgregar) {
+        int i = 0;
+        for (TipoRecurso aux : recursos) {
             if (aux.getNombre().equals(x.getNombre())) {
-                return aux.getCantidadRecurso();
+                checkearPeso(cantidadAgregar, i);
+            }
+            i++;
+        }
+    }
+
+    public void checkearPeso(int cantidadAgregar, int i) {
+        if (peso + cantidadAgregar < 30) {
+            recursos[i].setStack(recursos[i].getStack() + cantidadAgregar);
+            this.peso = this.peso + cantidadAgregar;
+        } else {
+            int exceso = peso + cantidadAgregar - 30;
+            int cantidadAgregarNueva = cantidadAgregar - exceso;
+            recursos[i].setStack(recursos[i].getStack() + cantidadAgregarNueva);
+            System.out.println("Se desperdiciaron " + exceso + " recursos ya que tienes un exceso de peso!");
+        }
+    }
+
+    public void verInventario() {
+        for (Recurso aux : this.recursos) {
+            System.out.println("*" + aux.getNombre() + " (" + aux.getStack() + ")");
+        }
+    }
+
+    public void soltarRecurso(String nombre, int cantidadTirar) {
+        int i = 0;
+        for (TipoRecurso aux : this.recursos) {
+            if (aux.getNombre().equals(nombre)) {
+                if (recursos[i].getStack() - cantidadTirar > 0) {
+                    recursos[i].setStack(recursos[i].getStack() - cantidadTirar);
+                    this.peso = this.peso - cantidadTirar;
+                } else {
+                    System.out.println("No puedes soltar mas de lo que tienes!");
+                }
+            }
+            i++;
+        }
+    }
+
+    public boolean usarDelInventario(String nombre, int cantidad){ //solo para usar en otras funciones!
+        for (Recurso aux:this.recursos) {
+            if (aux.getNombre().equals(nombre)) {
+                if((aux.getStack() - cantidad) > 0){
+                    aux.setStack(aux.getStack() - cantidad);
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
             }
         }
-        return 0;
+        return false;
     }
 }
